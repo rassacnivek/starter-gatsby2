40ff27cdf9f70f112e259272cb97f0ac8371387b
Kévin <kevin95cassar@gmail.com>
2020-04-17 16:57:28 +0200

    add: Contact form

---
+++ src/pages/contact.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+  render() {
+    return (
+      <form name="contact" method="POST" action="/success" data-netlify="true" netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    )
+  }
+}
+
+export default Contact;
---
+++ src/pages/success.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class success extends Component {
+  render() {
+    return (
+      <div>
+        Your message have been posted. Thank you !
+      </div>
+    )
+  }
+}
